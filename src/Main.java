50b672509d0b0147da941fb7e8c68011c5baef6e
Nehir0109 <nehir.cakilci@gmail.com>
2024-03-05 16:12:32 +0300

    Dizi Ortalaması

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 // Dizi Toplamı: Bir tamsayı dizisinin tüm elemanlarının toplamını bulan bir Java metodunu yazın.
 
-public class Main{
+/*public class Main{
     public static int toplamhesapla(int[] dizi) {
 
         int toplam =0;
@@ -20,5 +22,38 @@ public class Main{
         int toplam = toplamhesapla(dizi);
 
         System.out.println("Dizi Toplamı: "+ toplam);
+    }
+}*/
+
+// Dizi Ortalaması:Bir double dizisinin değerlerinin ortalamasını hesaplayan bir Java programı yazın
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner get= new Scanner(System.in);
+
+        System.out.print("Beş tane sayı giriniz: ");
+        double num1 = get.nextDouble();
+        double num2 = get.nextDouble();
+        double num3 = get.nextDouble();
+        double num4 = get.nextDouble();
+        double num5 = get.nextDouble();
+
+        double dizi[]={num1,num2,num3,num4,num5};
+
+        double ortalama = ortalama(dizi);
+
+        System.out.println("Dizinin ortalaması: "+ortalama);
+
+    }
+
+    public static double ortalama(double[] dizi) {
+
+        double toplam =0;
+
+        for(double sayı: dizi){
+            toplam += sayı;
+        }
+
+        return toplam / dizi.length;
     }
 }
