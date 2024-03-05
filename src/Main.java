0bdea943d624ac04776572e1186b8dba59dece7c
Nehir0109 <nehir.cakilci@gmail.com>
2024-03-05 15:50:56 +0300

    Dizi Toplamı

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
-public class Main {
+// Dizi Toplamı: Bir tamsayı dizisinin tüm elemanlarının toplamını bulan bir Java metodunu yazın.
+
+public class Main{
+    public static int toplamhesapla(int[] dizi) {
+
+        int toplam =0;
+
+        for(int eleman: dizi){
+            toplam+=eleman;
+        }
+
+        return toplam;
+
+    }
+
     public static void main(String[] args) {
+
+        int dizi[] = {1,2,3,4,5,6,7,8,9,10};
+
+        int toplam = toplamhesapla(dizi);
+
+        System.out.println("Dizi Toplamı: "+ toplam);
     }
 }
